fc89fa4faa6d6fb04807cb7f2cbae28dbbdeea90
lifthus <lifthus531@gmail.com>
2023-11-02 22:39:37 +0900

    TestBuildMatchTree

--- pathmatch_test.go
+++ pathmatch_test.go
@@ -1,33 +1,5 @@
 package pathmatch
 
-import "testing"
-
 var (
-	TestDel    = "/"
-	PathIntMap = map[string]int{
-		"/":      1,
-		"a/a":    2,
-		"/a/b/":  3,
-		"/a/b/c": 4,
-		"/a///c": 5,
-	}
+	TestDel = "/"
 )
-
-func TestBuildMatchMap(t *testing.T) {
-	// root, err := buildMatchTree[int](TestDel, PathIntMap)
-	// if err != nil {
-	// 	t.Errorf("buildMatchTree has failed: %v", err)
-	// }
-	// switch {
-	// case root.target != 1:
-	// 	fallthrough
-	// case root.next["a"].next["a"].target != 2:
-	// 	fallthrough
-	// case root.next["a"].next["b"].target != 3:
-	// 	fallthrough
-	// case root.next["a"].next["b"].next["c"].target != 4:
-	// 	fallthrough
-	// case root.next["a"].next["c"].target != 5:
-	// 	t.Errorf("buildMatchTree has built invalid tree: %v", root)
-	// }
-}
