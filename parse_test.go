0daee7dff6de55fd930d4a70be1ab553e2eec885
lifthus <lifthus531@gmail.com>
2023-11-02 22:48:42 +0900

    new test for splitFirstSegment

--- parse_test.go
+++ parse_test.go
@@ -18,3 +18,20 @@ func TestSplitFirstSegment(t *testing.T) {
 		}
 	}
 }
+
+func TestSplitFirstSegmentWithLongerSep(t *testing.T) {
+	QnA := map[string][]string{
+		"@#a@#b@#c":     {"a", "@#b@#c"},
+		"a@#b@#c":       {"a", "@#b@#c"},
+		"a@#b@#c@#":     {"a", "@#b@#c"},
+		"@#a@#b@#c@#@#": {"a", "@#b@#c"},
+		"a@#b@#c@#@#@":  {"a", "@#b@#c@#@#@"},
+	}
+
+	for q, a := range QnA {
+		seg, remain := splitFirstSegment(TestDel, q)
+		if seg != a[0] || remain != a[1] {
+			t.Errorf("%s is expected to be split into %s and %s, but got %s and %s", q, a[0], a[1], seg, remain)
+		}
+	}
+}
